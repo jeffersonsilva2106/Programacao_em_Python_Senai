d6822e87dcecfa61f1d42c9920c54a484b0803cb
jeffersonsilva2106 <jefferson.silva2106@gmail.com>
2025-10-11 12:34:09 -0300

    Add files via upload

---
+++ concatenaçao.py
@@ -0,0 +1,67 @@
+# # sinais aririmeticos
+# # + - / // % **0.5
+
+# numero1 = 10
+# n2 = 20
+
+# print(numero1 + n2)
+# print(numero1 - n2)
+# print(numero1 * n2)
+# print(numero1 / n2)
+
+
+# print(numero1 // n2)
+# print(numero1 % n2)
+
+
+
+
+# # sinais logicos
+
+
+# numero1 = 10
+# n2 = 2
+
+# print(numero1 > 2)
+# print(numero1 < 2)
+# print(numero1 >= 2)
+# print(numero1 <= 2)
+# print(numero1 != 2)
+# print(n1 == 2)
+
+# print('objeto', 10, n1) # output -- saida 
+
+
+# input('digite algo') # input -- entrada 
+# # o imput naturalmente gera um texto 
+
+# n1 = float(input('digite um numero: ')
+# n2 = int(input('digite um numero: ')
+# print(n1 + n2)
+
+# n1 = float(input('digite um numero: ')
+# n2 = int(input('digite um numero: ')
+# print(n1 - n2)
+
+# n1 = int(input('digite um numero: ')
+# n2 = int(input('digite um numero: ')
+# print(n1 / n2)
+
+# n1 = int(input('digite um numero: ')
+# n2 = int(input('digite um numero: ')
+# print(n1 * n2)
+
+
+
+
+
+# concatenaçao -- juntar dados
+
+nome = input('digite seu nome: ')
+idade = int(input('digite sua idade:'))
+endereço = input('digite seu endereço: ')
+curso = input('curso: ')
+salario = float(input('digite seu salario: '))
+
+print('nome', nome)
+
